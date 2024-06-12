cc634e3a5778f61975d53bee7c528fa6797aebb6
milkymilky0116 <sjlee990129@gmail.com>
2024-06-12 23:56:25 +0900

    chore: add field ignore on book entity

--- src/main/java/com/independentbooks/domain/book/domain/Book.java
+++ src/main/java/com/independentbooks/domain/book/domain/Book.java
@@ -1,5 +1,6 @@
 package com.independentbooks.domain.book.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.independentbooks.domain.common.BaseEntity;
 import com.independentbooks.domain.like.domain.Like;
 import com.independentbooks.domain.review.domain.Review;
@@ -12,7 +13,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@Table(name="Book")
+@Table(name = "Book")
 @NoArgsConstructor
 @Getter
 public class Book extends BaseEntity {
@@ -22,37 +23,38 @@ public class Book extends BaseEntity {
     @Column(name = "book_id")
     private Long bookId;
 
-    @Column(name="title", nullable = false) //도서 제목
+    @Column(name = "title", nullable = false) //도서 제목
     private String title;
 
-    @Column(name="author", nullable = false) //저자
+    @Column(name = "author", nullable = false) //저자
     private String author;
 
-    @Column(name="publisher", nullable = false) //출판사
+    @Column(name = "publisher", nullable = false) //출판사
     private String publisher;
 
-    @Column(name="isbn", nullable = false, unique = true) //ISBN
+    @Column(name = "isbn", nullable = false, unique = true) //ISBN
     private String isbn;
 
     // 타입 Text로 지정
-    @Column(name="book_info", columnDefinition = "TEXT", nullable = false) //도서 정보
+    @Column(name = "book_info", columnDefinition = "TEXT", nullable = false) //도서 정보
     private String bookInfo;
 
-    @Column(name="price", nullable = false) //도서 가격
+    @Column(name = "price", nullable = false) //도서 가격
     private Long price;
 
-    @Column(name="published_date", nullable = false) //출간일
+    @Column(name = "published_date", nullable = false) //출간일
     private LocalDateTime publishedDate;
 
-    @Column(name="image", nullable = false) //도서 표지 이미지
+    @Column(name = "image", nullable = false) //도서 표지 이미지
     private String image;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "book", fetch = FetchType.LAZY)
     private List<Review> reviews;
 
     @OneToMany(mappedBy = "book")
     private List<Like> likes;
-  
+
     // 컬렉션과 N:N 관계
 //     @ManyToMany(mappedBy = "books")
 //     private List<Collection> collections = new ArrayList<>();
